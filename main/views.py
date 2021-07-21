a0e2fd81baeddc016d0e8844f32f87a09fbd95b7
EstelleDa <baishenlonger@live.com.sg>
2021-07-21 11:33:00 +1000

    tessssting

--- main/views.py
+++ main/views.py
@@ -14,6 +14,7 @@ def get_top_n(n, ls):
     return [i for i, count in sorted(counter, key=lambda x: x[1])[-n:]]
 
 #hahahahha
+#lllll
 def home_view(request):
     # Tuples are required to allow search GET requests to be contructed from the
     # organism raw text instead of the formatted text. The other fields are
